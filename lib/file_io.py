a74c0f4919cec750577de7de39b16a4387a2b758
Alyssa De Los Santos <adelossantos6@gmail.com>
2024-03-10 15:08:50 -0500

    complete file io lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    file_path = f'{file_name}.txt'
+    with open(file_path, mode='w') as text_file:
+        text_file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    file_path = f'{file_name}.txt'
+    with open(file_path, mode='a') as text_file:
+        text_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_path = f'{file_name}.txt'
+    with open(file_path) as text_file:
+        file_content = text_file.read()
+    return file_content
+
+
+
